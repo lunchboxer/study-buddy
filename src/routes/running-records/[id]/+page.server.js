2832b58ec07515d5f30db397915e78979ebc34ca
James Smith <mrsmith@aliyun.com>
2024-08-20 19:23:26 +0800

    support adding and editing comments

--- src/routes/running-records/[id]/+page.server.js
+++ src/routes/running-records/[id]/+page.server.js
@@ -1,6 +1,7 @@
 import { client, sql } from '$lib/data'
 import { runningRecordUpdateSchema } from '$lib/schema'
 import { updateAction, deleteAction } from '$lib/server-utils'
+import { fail } from '@sveltejs/kit'
 
 export async function load({ params, locals }) {
   const runningRecordResult = await client.execute(
@@ -26,6 +27,16 @@ export async function load({ params, locals }) {
 
 export const actions = {
   delete: async ({ request }) => deleteAction(request, 'running_record'),
+  updateComment: async ({ request, params }) => {
+    const data = await request.formData()
+    const id = params.id
+    const comments = data.get('comments') || ''
+    const query = sql`UPDATE running_record SET comments = ${comments} WHERE id = ${id};`
+    const result = await client.execute(query)
+    if (result.rowsAffected === 0) {
+      return fail(500, { errors: { all: 'Could not update record' } })
+    }
+  },
   update: async ({ request }) =>
     updateAction(request, 'running_record', runningRecordUpdateSchema),
 }
--- src/routes/running-records/[id]/+page.svelte
+++ src/routes/running-records/[id]/+page.svelte
@@ -1,38 +1,37 @@
 <script>
-import { dateToLocal } from '$lib/utils'
-import TextArea from '$lib/text-area.svelte'
-import Form from '$lib/form.svelte'
-import WordToMark from '$lib/word-to-mark.svelte'
-import { page } from '$app/stores'
+  import { dateToLocal } from '$lib/utils'
+  import TextArea from '$lib/text-area.svelte'
+  import Form from '$lib/form.svelte'
+  import WordToMark from '$lib/word-to-mark.svelte'
+  import { page } from '$app/stores'
 
-export let data
+  export let data
 
-let words = data.runningRecord?.marked_text.split(/\s+/)
+  let words = data.runningRecord?.marked_text.split(/\s+/)
 
-const markAll = async type => {
-  let all = ''
-  if (type === 'unmarkAll') {
-    all = data.runningRecord?.marked_text.replace(/\[\/?[a-z]+\]/gm, '')
-    words = all.split(/\s+/)
-  }
-  if (type === 'allAccurate') {
-    words = data.runningRecord?.marked_text.split(/\s+/)
-    for (let i = 0; i < words.length; i++) {
-      words[i] =
-        `[accurate]${words[i].replace(/\[\/?[a-z]+\]/gm, '')}[/accurate]`
+  const markAll = async (type) => {
+    let all = ''
+    if (type === 'unmarkAll') {
+      all = data.runningRecord?.marked_text.replace(/\[\/?[a-z]+\]/gm, '')
+      words = all.split(/\s+/)
     }
-    all = words.join(' ')
-  }
+    if (type === 'allAccurate') {
+      words = data.runningRecord?.marked_text.split(/\s+/)
+      for (let i = 0; i < words.length; i++) {
+        words[i] = `[accurate]${words[i].replace(/\[\/?[a-z]+\]/gm, '')}[/accurate]`
+      }
+      all = words.join(' ')
+    }
 
-  const fullUrl = `${$page.url.href}/mark-word`
-  await fetch(fullUrl, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ all }),
-  })
-}
+    const fullUrl = `${$page.url.href}/mark-word`
+    await fetch(fullUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ all }),
+    })
+  }
 </script>
 
 <h1>Running Record for {data.runningRecord?.student_name}</h1>
@@ -66,6 +65,6 @@ const markAll = async type => {
   class="btn btn-success">All correct</button
 >
 
-<Form action="?/update">
+<Form action="?/updateComment">
   <TextArea label="Comments" data={data.runningRecord} name="comments" />
 </Form>
