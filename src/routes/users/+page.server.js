04daa8d57634a15f5f30e8d2ff4c626392edfbbd
James Smith <mrsmith@aliyun.com>
2024-08-23 23:32:59 +0800

    avoid sending password data

--- src/routes/users/+page.server.js
+++ src/routes/users/+page.server.js
@@ -6,10 +6,14 @@ export const load = async () => {
   const result = await client.execute(
     sql`SELECT * FROM user ORDER BY username;`,
   )
-  return { users: result?.rows || [] }
+  const resultUsers = result?.rows || []
+  const users = resultUsers.map(user => {
+    const { password, ...safeUser } = user
+    return safeUser
+  })
+  return { users }
 }
 
 export const actions = {
-  create: async ({ request }) =>
-    addAction(request, 'user', userCreateSchema),
+  create: ({ request }) => addAction(request, 'user', userCreateSchema),
 }
